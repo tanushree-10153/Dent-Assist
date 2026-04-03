bd9ae5445d89ef292dd0d7f25e9b0ba3782e9421
Tanushree-10153 <tanushreepabale51@gmail.com>
2026-04-03 16:33:19 +0530

    update db and api url

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,6 @@
-﻿const API = 'http://localhost:5000/api';
+﻿const API = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000/api'
+  : 'https://dent-assist-api.onrender.com/api';
 let token = localStorage.getItem('token');
 let userRole = localStorage.getItem('role');
 let userName = localStorage.getItem('name');
