42622a1872096d278b29b45f36959920e8560267
Tanushree-10153 <tanushreepabale51@gmail.com>
2026-04-05 23:21:09 +0530

    add SMTP timeout to prevent hanging on Render

--- backend/mailer.js
+++ backend/mailer.js
@@ -10,7 +10,10 @@ const transporter = nodemailer.createTransport({
   },
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 function sendMail(to, subject, html) {
